4a01c6a5f739f647ebe213dafc87014edd0674b7
Changwhi <whinada@gmail.com>
2023-06-10 09:36:34 -0700

    Implement button component
    To reuse a button component, I implemented a button component.
    CSS.module file included

--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState }from "react";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
+import Button from "../UI/Button";
+
 
 const AddUser = props => {
+    const [enterUserName, SetEnterUsername] = useState('');
+    const [enterAge,SetEnterAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+    };
+
+    const usernameChangeHandler = (event) => {
+        SetEnterUsername(event.target.value);
+        console.log(event.target.value);
+    };
+
+    const ageChangeHandler= (event) => {
+        SetEnterAge(event.target.value);
+        console.log(event.target.value);
     };
 
     return (
     <Card className={styles.input}>
     <form onSubmit={addUserHandler}>
         <label htmlFor='username'> User Name </label>
-        <input id='username' type="text"/>
+        <input id='username' type="text" onChange={usernameChangeHandler}/>
         <label htmlFor='username'> Age (Years) </label>
-        <input id='age' type="number"/>
-        <button type="submit"> Add User </button>
+        <input id='age' type="number" onChange={ageChangeHandler}/>
+        <Button type="submit"> Add User </Button>
     </form>
     </Card>
     )
